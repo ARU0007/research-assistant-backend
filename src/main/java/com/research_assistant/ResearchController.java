d88df1beeffb1cf329aacb4120c57ed823ffaf76
ARU0007 <aruneshagrahari95@gmail.com>
2025-03-30 22:55:48 +0530

    method processContent() created

--- src/main/java/com/research_assistant/ResearchController.java
+++ src/main/java/com/research_assistant/ResearchController.java
@@ -1,4 +1,19 @@
 package com.research_assistant;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class ResearchController {
+    private final ResearchService researchService;
+
+    public ResponseEntity<String> processContent(){
+        return "";
+    }
 }
